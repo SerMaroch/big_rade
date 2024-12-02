cc230f511a87e1aa79700b277d46d8560c26a594
SerMaroch <117520156+SerMaroch@users.noreply.github.com>
2024-12-02 22:21:57 +0500

    Обновить main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
 startbit.startbit_Init()
 basic.forever(function () {
-    while (false) {
-        if (input.pinIsPressed(TouchPin.P0) || input.pinIsPressed(TouchPin.P0)) {
+    for (let index = 0; index < 1000; index++) {
+        if (!(input.pinIsPressed(TouchPin.P0)) || !(input.pinIsPressed(TouchPin.P1))) {
             startbit.startbit_setMotorSpeed(60, 60)
-        } else if (!(input.pinIsPressed(TouchPin.P0)) || input.pinIsPressed(TouchPin.P0)) {
+        } else if (!(input.pinIsPressed(TouchPin.P0)) || input.pinIsPressed(TouchPin.P1)) {
             startbit.startbit_setMotorSpeed(40, -20)
-        } else if (input.pinIsPressed(TouchPin.P0) || !(input.pinIsPressed(TouchPin.P0))) {
+        } else if (input.pinIsPressed(TouchPin.P0) || !(input.pinIsPressed(TouchPin.P1))) {
             startbit.startbit_setMotorSpeed(-20, 40)
         }
+    }
+    startbit.startbit_setMotorSpeed(60, -60)
+    while (!(input.pinIsPressed(TouchPin.P0))) {
+    	
+    }
+    while (input.pinIsPressed(TouchPin.P0)) {
+    	
     }
 })
